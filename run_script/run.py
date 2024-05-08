671b502a8f987e8ee0998b8db6ef8941086ce879
Agustin Sanchez <asmerlinsky@gmail.com>
2024-05-07 21:09:19 -0300

    updated notebooks and the entrypoint to run the new model

--- run_script/run.py
+++ run_script/run.py
@@ -1,4 +1,8 @@
-from src.main import main
+from src.model.main import main
+from src.species_model.main import species_main
 
 if __name__ == "__main__":
-    r2_est, val_loss, train_loss, model_cpu = main()
+
+    # r2_est, val_loss, train_loss, model_cpu = main()
+
+    val_loss, train_loss, model_cpu = species_main()
