4c50ab269f4cd2ba08726510a54cf77b2053ce30
Agustin Sanchez <asmerlinsky@gmail.com>
2024-05-24 11:26:52 -0300

    refactored run script

--- run_script/run.py
+++ run_script/run.py
@@ -1,8 +1,12 @@
-from src.model.main import main
-from src.species_model.main import species_main
+""" Essentially used as entrypoint
+"""
+
+from plant_traits.model.main import main
+from plant_traits.species_model.main import species_main, test_lr
 
 if __name__ == "__main__":
 
     # r2_est, val_loss, train_loss, model_cpu = main()
 
     val_loss, train_loss, model_cpu = species_main()
+    # val_loss, train_loss = test_lr()
